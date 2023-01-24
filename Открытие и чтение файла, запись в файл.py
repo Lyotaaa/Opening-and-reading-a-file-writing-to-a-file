cb311c9ad898e55d33f0ff2262c09e8ad7a42ea2
Lyota <korystin.aleksei@yandex.ru>
2023-01-25 00:01:39 +0300

    new add

--- Открытие и чтение файла, запись в файл.py
+++ Открытие и чтение файла, запись в файл.py
@@ -15,4 +15,28 @@ with open('recipes.txt', 'rt', encoding='utf-8') as f:
             })
         f.readline()
         cook_book[dish_name] = indigents
-print(cook_book)
+# print(cook_book)
+
+#Задача №2
+def get_shop_list_by_dishes(dishes, person_count):
+    number_indigents = {}
+    for meal in dishes:
+        if meal in cook_book:
+            for indigents in cook_book[meal]:
+                #Свой вариант
+                indigents['quantity'] = int(indigents["quantity"]) * person_count
+                indigents = {
+                    'measure': indigents['measure'],
+                    'quantity': indigents['quantity'],
+                    'ingredient_name': indigents['ingredient_name']
+                }
+                number_indigents[indigents['ingredient_name']] = indigents
+                retrievable_value = indigents.pop('ingredient_name')
+                #Подсказал эксперт(аспират)
+                # number_indigents[indigents['ingredient_name']] = {
+                #     'measure': indigents['measure'],
+                #     'quantity': int(indigents['quantity']) * person_count
+                # }
+    return print(number_indigents)
+
+get_shop_list_by_dishes(['Омлет', 'Фахитос'], 10)
