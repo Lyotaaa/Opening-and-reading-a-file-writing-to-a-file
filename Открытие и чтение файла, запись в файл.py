3aeff2dfd9bdfc17e2545da79950b296aff9a807
Lyota <korystin.aleksei@yandex.ru>
2023-01-25 19:51:35 +0300

    new file

--- Открытие и чтение файла, запись в файл.py
+++ Открытие и чтение файла, запись в файл.py
@@ -1,28 +1,30 @@
 #Задача №1
-with open('recipes.txt', 'rt', encoding='utf-8') as f:
-    cook_book = {}
-    for line in f:
-        dish_name = line.strip()
-        ingredient_count = int(f.readline())
-        indigents = []
-        for i in range(ingredient_count):
-            emp = f.readline().strip()
-            ingredient_name, quantity, measure = emp.split(' | ')
-            indigents.append({
-                'ingredient_name': ingredient_name,
-                'quantity': quantity,
-                'measure': measure
-            })
-        f.readline()
-        cook_book[dish_name] = indigents
-# print(cook_book)
+def cook_book():
+    with open('recipes.txt', 'rt', encoding='utf-8') as f:
+        cook_book = {}
+        for line in f:
+            dish_name = line.strip()
+            ingredient_count = int(f.readline())
+            indigents = []
+            for i in range(ingredient_count):
+                emp = f.readline().strip()
+                ingredient_name, quantity, measure = emp.split(' | ')
+                indigents.append({
+                    'ingredient_name': ingredient_name,
+                    'quantity': quantity,
+                    'measure': measure
+                })
+            f.readline()
+            cook_book[dish_name] = indigents
+        return cook_book
 
 #Задача №2
 def get_shop_list_by_dishes(dishes, person_count):
+    book = cook_book()
     number_ingredients = {}
     for meal in dishes:
-        if meal in cook_book:
-            for ingredients in cook_book[meal]:
+        if meal in book:
+            for ingredients in book[meal]:
                 if ingredients['ingredient_name'] not in number_ingredients:
                     number_ingredients[ingredients['ingredient_name']] = {
                         'measure': ingredients['measure'],
@@ -36,6 +38,7 @@ def get_shop_list_by_dishes(dishes, person_count):
         else:
             print(f'\n"Такого блюда нет в списке!"\n')
     return number_ingredients 
-    
-    
+        
 print(get_shop_list_by_dishes(['Омлет', 'Фахитос'], 10))
+
+#Задача №3
